c2d68bf07bad43a9eedbb0ee513a837608fc51b2
cherkasoov <cherkasov5509@gmail.com>
2026-01-08 13:46:02 +0300

    [TASK-8] Add main

--- nikita.cherkasov/task-8/cmd/service/main.go
+++ nikita.cherkasov/task-8/cmd/service/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main()
+import (
+	"fmt"
+
+	"github.com/cherkasoov/task-8/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Println("load config error: %w", err)
+
+		return
+	}
+
+	fmt.Print(cfg.Environment, " ", cfg.LogLevel)
+}
